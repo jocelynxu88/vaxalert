40035a69c473f4f8e0fb9d0260b6665a9b0532b2
Nishckria Nataraja Babu <nishckrianatarajababu@Nishckrias-MBP.phub.net.cable.rogers.com>
2022-01-08 11:03:37 -0500

    Fixed footer files

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Footer.css';
+
+
+
+const Footer = () => {
+
+    return (
+        <footer>
+            <div class = 'Footer'>
+                <p> Footer </p> 
+            </div>
+
+        </footer>
+        
+    )
+    
+}
+
+export default Footer;
