d8d62245bff60544cf2f1c7ef29f27077116931d
jocelynxu88 <xu90@mcmaster.ca>
2022-01-08 10:51:03 -0500

    front page and dependencies

--- src/App.js
+++ src/App.js
@@ -1,30 +1,46 @@
-import React, { Component } from 'react';
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
-import SMSForm from './components/SMSForm';
+import Header from "./components/Header.js";
+import Form from "./components/Form.js";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import { Container, Row, Col } from 'react-grid-system';
+import SMSForm from "./components/SMSForm";
 
-class App extends Component {
-  render(){
+
+function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div>
+        <Header />
+        <div>
+        <Container>
+          <Row>
+            <Col md={8}>
+              <div className="intro">
+                <h1>Get notified.</h1>
+                <h1>Get vaccinated.</h1>
+                <Button variant="info">Start Now</Button>{' '}
+              </div>
+
+            </Col>
+            <Col md={4}>
+              <div className="cutegraphic">
+                <img alt="vaccinegraphic" src="https://images-na.ssl-images-amazon.com/images/I/8136d0VMhSL.jpg" />
+              </div>
+              
+            </Col>
+          </Row>
+        </Container>
+        </div>
+        <div>
+        <h1>hellllo</h1>
         <SMSForm />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        </div>
+      </div>
     </div>
   );
-  }
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+
+
+const Header = () =>{
+
+    return(
+        <nav>
+            <div> 
+                <img alt="logo" className="vaxlogo" src="https://marsdd.com/wp-content/uploads/2020/12/Vaccine.jpg" />
+            </div>
+        </nav>
+    )
+}
+
+export default Header;
