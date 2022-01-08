9f52973243e48952207fe88c2c53f197db219ae5
Kailin <68765813+kailinchu@users.noreply.github.com>
2022-01-08 10:37:10 -0500

    Fix react to show app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App';
 
-
-
-ReactDOM.render(React.createElement("h1", null, "Hello!"),
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
